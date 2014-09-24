458036f9a6f6d2b9329a0de1ca8b8d9e4b4ca9c7
Andrew Chavez <adchavez@gmail.com>
2014-09-24 13:09:52 -0500

    Insert ads, Cox metrics code

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-title" content="Prep Scores">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <link rel="apple-touch-icon" href="//projects.statesman.com/common/appicon_aas.jpg">
+
+  <link rel="icon" type="image/png" href="//projects.statesman.com/common/favicon.ico">
+
+  <meta name="description" content="">
+  <meta property="og:title" content="Texas High School Football Scores"/>
+  <meta property="og:description" content=""/>
+  <meta property="og:image" content="http://projects.statesman.com/common/appicon_aas.jpg"/>
+  <meta property="og:url" content="http://projects.statesman.com/databases/prep-football-scores/"/>
+
+  <title>Texas High School Football Scores</title>
+
+  <link href="/dist/styles.css" rel="stylesheet" />
+
+  <?php include "../../common/metrics-head.js";?>
+
+  <!-- Advertising -->
+  <script type='text/javascript'>
+  var googletag = googletag || {};
+  googletag.cmd = googletag.cmd || [];
+  (function() {
+    var gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+    gads.src = (useSSL ? 'https:' : 'http:') +
+    '//www.googletagservices.com/tag/js/gpt.js';
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+  })();
+  </script>
+
+  <script type='text/javascript'>
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/12523293/www.statesman.com-wired/sports/high_school_football', [728, 90], 'div-gpt-ad-1403295829614-1').addService(googletag.pubads());
+    googletag.defineSlot('/12523293/www.statesman.com-wired/sports/high_school_football', [320, 50], 'div-gpt-ad-1403295829614-3').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().enableVideoAds();
+    googletag.enableServices();
+  });
+  </script>
+</head>
+<body>
+
+	<nav class="top-bar">
+		<ul class="title-area">
+			<li class="name"><h1>
+				<a href="http://www.statesman.com/" target="_blank"><img src="//projects.statesman.com/common/white-logo-small.png"></a>
+			</h1></li>
+		</ul>
+	</nav>
+
+  <div class="row small-11 medium-8 large-6 centered">
+
+    <h1>High school football scores</h1>
+
+    <noscript>
+      <div data-alert class="alert-box alert radius">
+        This app requires JavaScript. To view scores, enable JavaScript in your Web browser and reload this page.
+      </div>
+    </noscript>
+
+    <form class="hide">
+      <select id="week">
+        <option value="2014-08-29">Week 1</option>
+        <option value="2014-09-05">Week 2</option>
+        <option value="2014-09-12">Week 3</option>
+        <option selected value="2014-09-19">Week 4</option>
+      </select>
+    </form>
+
+    <table class="scoreboard hide">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Away</th>
+          <th>Home</th>
+        </tr>
+      </thead>
+      <tbody id="scores"></tbody>
+    </table>
+
+    <div id="stories"></div>
+
+    <hr />
+
+  </div>
+
+  <!-- Advertising/metrics -->
+  <div class="text-center advertising">
+    <div id='div-gpt-ad-1403295829614-3' class="show-for-small-only">
+      <script type='text/javascript'>
+        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1403295829614-3'); });
+      </script>
+    </div>
+    <div id='div-gpt-ad-1403295829614-1' class="show-for-medium-up">
+      <script type='text/javascript'>
+        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1403295829614-1'); });
+      </script>
+    </div>
+  </div>
+
+  <div id="footer" class="row small-11 medium-8 large-6 centered">
+    <?php include "../../common/footer.php"; ?>
+  </div>
+
+  <!-- statesman specfic stuff -->
+  <script type="text/javascript">
+    var projectSite = "statesman";
+    var projectChannel = "sports";
+    var projectCategory = "high school football";
+    var projectSubCategory = "mobile scores";
+  </script>
+
+  <?php include "../../common/metrics.js"; ?>
+
+  <script src="/dist/scripts.js" ></script>
+
+</body>
+</html>
