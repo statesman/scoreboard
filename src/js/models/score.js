f806c985b33be596dfc2d8e7c5c587de8851d9f1
Andrew Chavez <adchavez@gmail.com>
2014-10-01 13:24:24 -0500

    Exclude unused TeamPlayer data; Move some processing from client to server (#24)

--- src/js/models/score.js
+++ src/js/models/score.js
@@ -1,32 +1,9 @@
-define(['moment', 'backbone', 'modules/favs'], function(moment, Backbone, Favs) {
+define(['backbone', 'modules/favs'], function(Backbone, Favs) {
 
   var favs = new Favs();
 
   var Score = Backbone.Model.extend({
     initialize: function() {
-
-      // Add a boolean for district games
-      if(this.get('AwayDistrictID') === this.get('HomeDistrictID')) {
-        this.set('DistrictGame', true);
-      }
-
-      // Make GameScoreIsFinal a boolean for easier templating
-      if(this.get('GameScoreIsFinal') === "1") {
-        this.set('GameScoreIsFinal', true);
-      }
-      else {
-        this.set('GameScoreIsFinal', false);
-      }
-
-      // Set a boolean for the winner that can be used during templating
-      if(this.get('GameScoreIsFinal')) {
-        if(this.get('AwayTeamScore') > this.get('HomeTeamScore')) {
-          this.set('AwayTeamWon', true);
-        }
-        else {
-          this.set('HomeTeamWon', true);
-        }
-      }
 
       // Check localstorage to see if teams are favs and store a boolean
       // on their model if they are
@@ -36,10 +13,6 @@ define(['moment', 'backbone', 'modules/favs'], function(moment, Backbone, Favs) 
       if(favs.isFav(this.get('AwayTeamID'))) {
         this.set('AwayTeamFav', true);
       }
-
-      // Store a Moment object with the game start time
-      var time = moment(this.get('GameDate') + this.get('GameTime'), "YYYY-MM-DDHH:mm:ss");
-      this.set('GameTimestamp', time.unix());
 
       // Store a lowercase version of each team name for searching
       var searchable = [this.get('HomeTeamName').toLowerCase(), this.get('AwayTeamName').toLowerCase()];
